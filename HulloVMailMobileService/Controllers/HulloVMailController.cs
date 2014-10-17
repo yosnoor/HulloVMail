4faedcaa8bc09efafbc3e9f4afceef4d54d9332a
Yos Noor <yosnoor@hotmail.com>
2014-10-17 22:14:16 +0100

    Changed the ridiculous names
    Changes

---
+++ HulloVMailMobileService/Controllers/HulloVMailController.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.OData;
+using Microsoft.WindowsAzure.Mobile.Service;
+using HulloVMailMobileService.DataObjects;
+using HulloVMailMobileService.Models;
+
+namespace HulloVMailMobileService.Controllers
+{
+    public class HulloVMailController : TableController<HulloVMail>
+    {
+        protected override void Initialize(HttpControllerContext controllerContext)
+        {
+            base.Initialize(controllerContext);
+            MobileServiceContext context = new MobileServiceContext();
+            DomainManager = new EntityDomainManager<HulloVMail>(context, Request, Services);
+        }
+
+        // GET tables/HulloVMail
+        public IQueryable<HulloVMail> GetAllHulloVMails()
+        {
+            return Query();
+        }
+
+        // GET tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public SingleResult<HulloVMail> GetHulloVMail(string id)
+        {
+            return Lookup(id);
+        }
+
+        // PATCH tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task<HulloVMail> PatchHulloVMail(string id, Delta<HulloVMail> patch)
+        {
+            return UpdateAsync(id, patch);
+        }
+
+        // POST tables/TodoItem
+        public async Task<IHttpActionResult> PostHulloVMail(HulloVMail item)
+        {
+            HulloVMail current = await InsertAsync(item);
+            return CreatedAtRoute("Tables", new { id = current.Id }, current);
+        }
+
+        // DELETE tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task DeleteHulloVMail(string id)
+        {
+            return DeleteAsync(id);
+        }
+    }
+}
---
+++ HulloVMailMobileService/DataObjects/HulloVMail.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.WindowsAzure.Mobile.Service;
+using System;
+
+namespace HulloVMailMobileService.DataObjects
+{
+    public class HulloVMail : EntityData
+    {
+        public int HulloID { get; set; }
+        public string From { get; set; }
+        public DateTime RecordedDate { get; set; }
+        public byte[] Message { get; set; }
+        public bool IsNew { get; set; }
+        public string EmailID { get; set; }
+        public string FromDisplay { get; set; }
+    }
+}
